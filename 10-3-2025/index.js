d08662676fba6e689473b1773cda22edec8bd27c
MANH NGUYEN DUC <135792849+manhwork@users.noreply.github.com>
2025-03-10 16:32:48 +0700

    Tìm hiểu mục 1,2,3

---
+++ 10-3-2025/index.js
@@ -0,0 +1,23 @@
+/**
+ * Mutation và Inmutation
+ */
+
+// Mutation : biến đổi biến trực tiếp
+// Inmutation : Không thay đổi biến trực tiếp mà tạo ra bản sao mới khi có sự thay đổi
+
+/**
+ * React Dom
+ */
+
+// Virtual DOM : bản sao dom thật
+// Reconciliation (Quá trình đối chiếu) Cơ chế phát hiện sự thay đổi giữa dom ảo và dom thật
+
+// Cơ chế :
+// Tạo bản sao dom ảo từ dom thật --> so sáng cấu trúc dom ảo và dom thật --> Cập nhật dom thật
+
+/**
+ * useref
+ */
+
+// Truy xuất 1 phần tử trong do thật nhưng không làm re-render
+// Lưu trữ giá trị có thể thay đổi nhưng ko gây re-render khi cập nhật
